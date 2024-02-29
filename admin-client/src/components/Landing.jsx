c4d24325ae4b1d2f6315b1723f24ddf3c0968346
shaikhmubin02 <shaikhmubin572@gmail.com>
2024-03-01 04:09:17 +0530

    init

--- admin-client/src/components/Landing.jsx
+++ admin-client/src/components/Landing.jsx
@@ -44,7 +44,7 @@ export const Landing = () => {
                 </div>
             </Grid>
             <Grid item xs={12} md={6} lg={6}  style={{marginTop: 20}}>
-                <img src={"/class.jpeg"} width={"100%"} />
+                <img src={"/class.jpg"} width={"100%"} style={{borderRadius: 10}}/>
             </Grid>
         </Grid>
     </div>
